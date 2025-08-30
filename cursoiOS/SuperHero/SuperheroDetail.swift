c777add5d2a62f8cff83885168464e4fcf36ab36
Will <guillermo300@outlook.es>
2025-08-30 14:24:16 -0600

    Project updated

---
+++ cursoiOS/SuperHero/SuperheroDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SuperheroDetail.swift
+//  cursoiOS
+//
+//  Created by will on 30/08/25.
+//
+
+import SwiftUI
+
+struct SuperheroDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuperheroDetail()
+}
